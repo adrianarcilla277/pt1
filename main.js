6066ecfc0e224ca41a059d34d3c0fe0d42098d54
adrianarcilla277 <165882630+adrianarcilla277@users.noreply.github.com>
2024-04-03 15:30:16 +0800

    PT1

---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  document.getElementById('myImage').src = '1.jpg'
+  document.getElementById('descript').style.display = 'none'
+}
+
+function myFunction1() {
+  document.getElementById('myImage').src = '2.jpg'
+  document.getElementById('descript').style.display = 'block'
+}
